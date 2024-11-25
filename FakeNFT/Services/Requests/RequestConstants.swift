ce9f5d8769c4f26ee23479726c572e38301a5d33
Giya077 <giyaDevIos@gmail.com>
2024-11-26 01:00:36 +0500

    RequestConst done

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,7 +1,5 @@
 enum RequestConstants {
-    enum RequestConstants {
-        static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-        #warning("Instert your token here")
-        static let token = "a66690d9-233c-4541-a539-179c0a04d8da"
-    }
+    static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
+    #warning("Instert your token here")
+    static let token = "a66690d9-233c-4541-a539-179c0a04d8da"
 }
